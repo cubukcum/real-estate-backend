df42da1e516bc118d25ddd174f6934752a8be3a4
Mehmet Sait Cubukcu <109916532+cubukcum@users.noreply.github.com>
2024-12-17 22:47:55 +0300

    added admins table

--- scripts/dbInit.js
+++ scripts/dbInit.js
@@ -36,6 +36,13 @@ VALUES
 ON CONFLICT DO NOTHING;
 `;
 
+const createAdminTable = `
+CREATE TABLE admins (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255) UNIQUE NOT NULL,
+  password VARCHAR(255) NOT NULL
+);`;
+
 const initializeDatabase = async () => {
   try {
     const client = await pool.connect();
