236d5d608286bec2703ce2bfc7c4dba8e76e89ab
Mehmet Sait Cubukcu <109916532+cubukcum@users.noreply.github.com>
2024-12-29 00:34:39 +0300

    updated cors option in the right file

--- server.js
+++ server.js
@@ -6,18 +6,23 @@ require("dotenv").config();
 const app = express();
 const projectRoutes = require("./routes/projects");
 const adminRoutes = require("./routes/admin");
-const uploadRoutes = require("./routes/uploadRoutes");
 
 // Middleware
 app.use(bodyParser.json());
 
 // Configure CORS
 const corsOptions = {
-  origin: "http://localhost:3000", // Allow requests from this origin
-  methods: ["GET", "POST", "PUT", "DELETE"], // Specify allowed HTTP methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Specify allowed headers
+  origin: "https://ozsedir.vercel.app",
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  preflightContinue: false,
+  optionsSuccessStatus: 204
 };
 app.use(cors(corsOptions));
+
+// Add custom OPTIONS handler for preflight requests
+app.options('*', cors(corsOptions));
 
 const authMiddleware = require("./middleware/auth");
 
@@ -27,8 +32,6 @@ app.use("/admin/dashboard", authMiddleware, adminRoutes);
 // Routes
 app.use("/projects", projectRoutes);
 app.use("/admin", adminRoutes);
-app.use("/upload", uploadRoutes);
-
 
 // Start the server
 const PORT = process.env.PORT || 5000;
