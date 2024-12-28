c2c4a98db547668c50a740b9b0b9418fc2a78636
Mehmet Sait Cubukcu <109916532+cubukcum@users.noreply.github.com>
2024-12-28 16:42:15 +0300

    cloudflare r2 config file

---
+++ config/r2.js
@@ -0,0 +1,20 @@
+const { S3Client } = require('@aws-sdk/client-s3');
+
+const R2_ACCOUNT_ID = process.env.R2_ACCOUNT_ID;
+const R2_ACCESS_KEY_ID = process.env.R2_ACCESS_KEY_ID;
+const R2_SECRET_ACCESS_KEY = process.env.R2_SECRET_ACCESS_KEY;
+const R2_BUCKET_NAME = process.env.R2_BUCKET_NAME;
+
+const r2Client = new S3Client({
+    region: 'auto',
+    endpoint: `https://${R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    credentials: {
+        accessKeyId: R2_ACCESS_KEY_ID,
+        secretAccessKey: R2_SECRET_ACCESS_KEY,
+    },
+});
+
+module.exports = {
+    r2Client,
+    R2_BUCKET_NAME
+};
